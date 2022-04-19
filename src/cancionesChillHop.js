03d1e3b8e2f4840d189f78edcad9aaa66fb08f6d
montanojd <gpapplica@gmail.com>
2022-04-19 11:16:01 -0500

    first commit

---
+++ src/cancionesChillHop.js
@@ -0,0 +1,89 @@
+import { v4 as uuidv4 } from 'uuid';
+
+function cancionesChillHop() {
+  return [
+    {
+      id: uuidv4(),
+      nombre: "528 Hz - DNA Upgrade (20 Minute) Meditation",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia801504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528%20Hz%20-%20DNA%20Upgrade%20%2820%20Minute%29%20Meditation.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+    {
+      id: uuidv4(),
+      nombre: "02 Hour 528 Hz Heart Chakra Regeneration",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia801504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528%20Hz%20Heart%20Chakra%20Regeneration%20_%20Repair%20%26%20Heal%20The%20Heart%20Chakra%20_%20Open%20%26%20Balance%20ANAHATA%20Chakra.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+    {
+      id: uuidv4(),
+      nombre: "03 Hour 528 Hz Positive Transformation",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia601504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528%20Hz%20Positive%20Transformation%2C%20Emotional%20%26%20Physical%20Healing%2C%20Anti%20Anxiety%2C%20Rebirth.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+    {
+      id: uuidv4(),
+      nombre: "09 Hour 528 Hz  DREAMSCAPE for POSITIVE TRANSFORMATION",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia801504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528%20Hz%20_%20DREAMSCAPE%20for%20POSITIVE%20TRANSFORMATION%20_%20Raise%20Positive%20Vibrations.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+    {
+      id: uuidv4(),
+      nombre: "02 Hour 528Hz  Release Inner Conflict & Struggle",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia601504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528Hz%20%20Release%20Inner%20Conflict%20%26%20Struggle%20_%20Anti%20Anxiety%20Cleanse%20-%20Stop%20Overthinking%2C%20Worry%20%26%20Stress.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+    {
+      id: uuidv4(),
+      nombre: "02 Hour 528Hz Energy CLEANSE Yourself & Your Home",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia801504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528Hz%20Energy%20CLEANSE%20Yourself%20%26%20Your%20Home%20-%20Heal%20Old%20Negative%20Energies%20From%20Your%20House%20Frequency.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+    {
+      id: uuidv4(),
+      nombre: "03 Hour 528Hz Music To Manifest Miracles Into Your Life",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia801504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528Hz%20Music%20To%20Manifest%20Miracles%20Into%20Your%20Life%20_%20Deep%20Positive%20Energy%20-%20Release%20Negative%20Vibes.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+    {
+      id: uuidv4(),
+      nombre: "01 Hour  528Hz ",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia801504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528Hz%20Regeneraci%C3%B3n%20completa%20del%20cuerpo%20-%20Curaci%C3%B3n%20completa%20del%20cuerpo%20-%20Cura%20F%C3%ADsica%20y%20Emociona.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+    {
+      id: uuidv4(),
+      nombre: "02 Hour 528Hz _ Open Heart Chakra",
+      portada: "https://image.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_53876-77451.jpg", 
+      artista: "528Hz Sounds",
+      audio: "https://ia801504.us.archive.org/28/items/528hz_202103/%5BMP3FY%5D%20528Hz%20_%20Open%20Heart%20Chakra%20%E2%9E%A4%20Love%20Frequency%20528hz%20Music%20_%20528hz%20Heart%20Chakra%20Activation%20-%20528hz%20Love.mp3",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true 
+    },
+  ];
+}
+
+export default cancionesChillHop;
